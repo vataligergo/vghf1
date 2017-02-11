74e4bb6391ce2486eadeafc02002a94a670d8172
Gergö <Gerg@GERG-PC>
2017-02-11 12:52:29 +0100

    class többnyire kész

---
+++ szamologep/szamologep/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamologep
+{
+    class Calculator
+    {
+        // +
+        public static int Osszead(int egyikszam, int masikszam)
+        {
+            return egyikszam + masikszam;
+        }
+        // -
+        public static int Kivon(int egyikszam, int masikszam)
+        {
+            return egyikszam - masikszam;
+        }
+        // *
+        public static int Szorzas(int egyikszam, int masikszam)
+        {
+            return egyikszam * masikszam;
+        }
+        // /
+        public static int Osztas(int egyikszam, int masikszam)
+        {
+            return egyikszam / masikszam;
+        }
+
+        // C
+        public static int Torol(int egyikszam, int masikszam)
+        {
+            return 0;
+        }
+
+        // CE
+        public static int FullTorol(int egyikszam, int masikszam)
+        {
+            return 0; ;
+        }
+    }
+}
--- szamologep/szamologep/Form1.cs
+++ szamologep/szamologep/Form1.cs
@@ -16,5 +16,71 @@ namespace szamologep
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) //0
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button2_Click(object sender, EventArgs e) //1
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button8_Click(object sender, EventArgs e) //2
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button7_Click(object sender, EventArgs e) //3
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button6_Click(object sender, EventArgs e) //4
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e) //5
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button4_Click(object sender, EventArgs e)//6
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button3_Click(object sender, EventArgs e)//7
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button9_Click(object sender, EventArgs e)//8
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button10_Click(object sender, EventArgs e)//9
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button17_Click(object sender, EventArgs e) // +
+        {
+            
+        }
     }
 }
